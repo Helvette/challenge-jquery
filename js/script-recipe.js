7eea7c7a80ca8e70276bd3bc8633168a2f740e4f
Helvette <malastrugo@live.cl>
2018-01-08 18:31:11 -0300

    Etapa 8: dándole funcionalidad a los botones de recipehtml

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -2,4 +2,12 @@ $(document).ready( function(){
 	$(".js-back").show();
 	$(".js-menu").hide();
 	console.log("Imprimiendo mensaje en consola al cargar el documento.");
+
+	$(".js-show-recipe").click(function(){
+		$(".page").removeClass("make");
+	})
+
+	$(".js-show-make").click(function(){
+		$(".page").addClass("make");
+	})
 })
