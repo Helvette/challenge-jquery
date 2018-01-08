bd20f491d6a730ca539236e8fca74e4112393310
Helvette <malastrugo@live.cl>
2018-01-08 18:45:24 -0300

    Etapa 9: agregando y quitando clases active en botones de recipehtml

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -1,13 +1,17 @@
 $(document).ready( function(){
 	$(".js-back").show();
-	$(".js-menu").hide();
+	$(".js-menu").remove();
 	console.log("Imprimiendo mensaje en consola al cargar el documento.");
 
 	$(".js-show-recipe").click(function(){
 		$(".page").removeClass("make");
+		$(this).addClass("active");
+		$(".js-show-make").removeClass("active");
 	})
 
 	$(".js-show-make").click(function(){
 		$(".page").addClass("make");
+		$(this).addClass("active");
+		$(".js-show-recipe").removeClass("active");
 	})
 })
