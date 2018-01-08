b41649f8a96250e761c3de9c36e8dc2f8404a8f7
Helvette <malastrugo@live.cl>
2018-01-08 17:46:51 -0300

    Etapa 4: generar contenido en la sección de SEE WHAT'S COOKING

--- js/script-index.js
+++ js/script-index.js
@@ -8,6 +8,7 @@ $(document).ready( function(){
 		$(element).text("NUEVAS RECETAS");
 	}
 	printNews(".new-recipe");
+	//renderActivities(activitiesArray);
 });
 
 
@@ -16,23 +17,41 @@ $(document).ready( function(){
 * marcado el atributo "highlighted" como TRUE
 */
 function renderHighlightedRecipes(recipesArray) {
-	console.log('Recipes: ', recipesArray);
+	//console.log('Recipes: ', recipesArray);
 	for(var i=0; i < recipesArray.length; i++){
 		if(recipesArray[i].highlighted == true){
-			//renderRecipe(recipeArray[i]);
-			console.log(recipesArray[i]);
+			renderRecipe(recipesArray[i]);
 		}
 	}
 }
 
+
 /*
-* Función que se encarga de pintar UNA recetas que tenga 
+* Función que se encarga de pintar UNA recetas que tenga
 * marcado el atributo "highlighted" como TRUE
-* Aqui se tiene que crear el HTML que esta en el 
+* Aqui se tiene que crear el HTML que esta en el
 * archivo "templates/templates-recipe.html"
 */
 function renderRecipe(recipe) {
 	console.log('Voy a pintar la receta: ', recipe);
+	var title = recipe.title;
+	var name = recipe["source"].name;
+	var imageUrl = recipe.name;
+	imageUrl = "img/recipes/640x800/" + imageUrl + ".jpg";
+	var html = "<a class='item-recipe' href='#'>"+
+					"<span class='attribution'>"+
+ 					"<span class='title-recipe'>"+ title + "</span>"+
+					"<span class='metadata-recipe'>"+
+					"<span class='author-recipe'>"+ name + "</span>" +
+					"<span class='bookmarks-recipe'>" +
+					"<span class='icon-bookmark'></span>"+
+					"</span>"+
+					"</span>"+
+					"</span>"+
+					"<img src='"+ imageUrl + "'/>"+
+				"</a>";
+	$(".list-recipes").append(html);
+	console.log(imageUrl)
 }
 
 
@@ -42,6 +61,12 @@ function renderRecipe(recipe) {
 */
 function renderActivities(activitiesArray) {
 	console.log('Activities: ', activitiesArray);
+	if(activitiesArray.length < 0){
+		$(".wrapper-message").hide();
+	}
+	for(var i=0; i < activitiesArray.length; i++){
+		renderActivity(activitiesArray[i]);
+	}
 }
 
 /*
