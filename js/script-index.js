924790ade40313edc79f23728b6c44535bff9167
Helvette <malastrugo@live.cl>
2018-01-08 09:54:03 -0300

    Etapa 1: esconder y mostrar íconos dependiendo de dónde nos encontramos

--- js/script-index.js
+++ js/script-index.js
@@ -2,7 +2,8 @@ $(document).ready( function(){
 
 	//La variable "recipesArray" esta declarada en el archivo "data/recipes.js"
 	renderHighlightedRecipes(recipesArray);
-
+	$(".js-back").hide();
+	$(".js-menu").show();
 });
 
 
