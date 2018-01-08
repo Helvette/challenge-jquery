68c30f669391d9f5c59f05e4785442e0cf87fac7
Helvette <malastrugo@live.cl>
2018-01-08 18:50:12 -0300

    Etapa 10: dándole funcionalidad al botón flecha hacia atrás

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -2,16 +2,28 @@ $(document).ready( function(){
 	$(".js-back").show();
 	$(".js-menu").remove();
 	console.log("Imprimiendo mensaje en consola al cargar el documento.");
-
+	/*
+	* dándole click al botón RECIPE
+	*/
 	$(".js-show-recipe").click(function(){
 		$(".page").removeClass("make");
 		$(this).addClass("active");
 		$(".js-show-make").removeClass("active");
 	})
-
+	/*
+	* dándole click al botón MAKE IT
+	*/
 	$(".js-show-make").click(function(){
 		$(".page").addClass("make");
 		$(this).addClass("active");
+		$(".js-show-recipe").removeClass("active");
+	})
+	/*
+	* dándole click a la fecha hacia atrás
+	*/
+	$(".js-back").click(function(){
+		$(".page").addClass("make");
+		$(".js-show-make").removeClass("active");
 		$(".js-show-recipe").removeClass("active");
 	})
 })
